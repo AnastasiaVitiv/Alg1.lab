a60d3d89fb11ab40586b7b619093a8b6909ccccf
Vitiv Anastasia <vitiv.tuska@gmail.com>
2024-09-09 10:59:44 +0300

    clear main

--- request_example.py
+++
@@ -1,7 +0,0 @@
-import requests
-# URL, до якого ви хочете виконати HTTP-запит
-url = "https://httpbin.org/get"  # або будь-який інший URL
-# Виконання GET-запиту до вказаного URL
-response = requests.get(url)
-# Виведення вмісту відповіді на екран
-print(response.text)
