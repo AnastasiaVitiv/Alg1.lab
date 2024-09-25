a77161f1b311ed206fcc8627fc31d9df20ca574d
AnastasiaVitiv <vitiv.tuska@gmail.com>
2024-09-25 10:35:12 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,63 +1,23 @@
-x = float(input("Введіть значення х"))
+x = float(input("Введіть значення х "))
 import math
 
-def get_lg_x(x):
-    x = (math.e ** x) + 4
-    if x == 0:
-        return "Lg is undefined for this value"
-    lg_x = math.log10(x)
-    return lg_x
+a = 4
+b = 6
+h = 0.2
+mn = 4.5
+mx = 5
+def calculate(x):
+    if x < mn and x >= a:
+        return 1 / math.sin(x ** 2)
 
-def run_task_12(a, b, h):
-    x = a
-    print("\nТаблиця значень для lg(x):")
-    print(f"{'x':^10} {'lg(x)':^15}")
-    while x <= b:
-        print(f"{x:^10.2f} {get_lg_x(x):^15.5f}")
-        x += h
+    elif x >= mn and x < mx:
+        return x + math.log(math.sqrt(x ** 7))
 
-def get_cosec_sqr_x(x):
-    sqr_x = x**2
-    sinus_sqr_x = math.sin(sqr_x)
-    if sinus_sqr_x == 0:
-        return "Cosec is undefined for this angle"
-    cosec_sqr_x = 1 / sinus_sqr_x
-    return cosec_sqr_x
+    elif x >= mx and x <= b:
+        return math.log10((math.e ** x) + 4)
 
-def run_task_11(a, b, h):
-    x = a
-    print("\nТаблиця значень для cos(x):")
-    print(f"{'x':^10} {'cos(x)':^15}")
-    while x <= b:
-        print(f"{x:^10.2f} {get_cosec_sqr_x(x):^15.5f}")
-        x += h
+    else:
+        return "Не входить в область значень"
 
-
-
-def get_lg_x(x):
-    x = (math.e ** x) + 4
-    if x == 0:
-        return "Lg is undefined for this value"
-    lg_x = math.log10(x)
-    return lg_x
-
-def run_task_13(a, b, h):
-    x = a
-    print("\nТаблиця значень для lg(x):")
-    print(f"{'x':^10} {'lg(x)':^15}")
-    while x <= b:
-        print(f"{x:^10.2f} {get_lg_x(x):^15.5f}")
-        x += h
-
-if x < 4.5 and x >= 4:
-    run_task_11(4, 6, 0.2)
-
-
-elif x >= 4.5 and x < 5:
-    run_task_12(4, 6, 0.2)
-
-elif x >= 5 and x <= 6:
-    run_task_13(4, 6, 0.2)
-
-else:
-    print("Не входить в область значень")
+result = calculate(x)
+print(f"Результат: {result}")
