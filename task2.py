c01fbdcd8c7065e6cceb943f2ce2f74db0301c1e
Vitiv Anastasia <vitiv.tuska@gmail.com>
2024-09-25 10:26:55 +0300

    Скласти блок-схеми алгоритмів та програми мовою Python для табулювання функцій. Дані для роботи беруться з таблиць 1 і 2 за вказівкою викладача: 	а) табулювання фукції (див. табл.1), що вибирається залежно від значення аргумента, на проміжку [a, b] з кроком табуляції h.

--- task2.py
+++ task2.py
@@ -1,42 +1,17 @@
-"""
-def sigma_x(x):
-    #infinity = float('inf')
-    total = 0.0
-    for k in range(1, 1024):
-        total += (1 / float(k)) * (math.tan(x / (2 ** float(k))))
-    return total
-
-def run_task_2(a, b, h):
-    x = a
-    print("\nТаблиця значень для sigma_x(x):")
-    print(f"{'x':^10} {'sigma_x(x)':^15}")
-    while x <= b:
-        print(f"{x:^10.2f} {sigma_x(x) :^15.5f}")
-        x += h
-
-
-
-"""
 import math
 
-x = float(input("Введіть значення x: "))
 d = 0.001
-total = 0.0
-k = 1.0
+term = 0.00
 h = 0.1
+a = 3
+b = 4
 
-if 3 <= x <= 4:
-    print("Значення підходить")
-    print(f"{'k':^10} {'term':^15} {'total':^15}")
+print(f"{'k':^10} {'x':^10} {'term':^15}")
 
-    while True:
-        term = abs((1 / float(k)) * (math.tan(x / (2 ** float(k)))))
-        if term <= d:
-            break
-        total += term
-        print(f"{k:^10} {term:^15.5f} {total:^15.5f}")
-        k += h
-
-    print(f"\nФінальна сума: {total:.5f}")
-else:
-    print("Значення x має бути в межах [3, 4].")
+k = 1
+x = a
+while (term > d or term is 0.00) and x <= b:
+    print(f"{k:^10} {x:^10.1f} {term:^15.5f}")
+    term += abs((1 / k) * (math.tan(x / (2 ** k))))
+    k += 1
+    x += h
