92b7d9a17ca9162d4983a0e97a2af73a383983f1
AnastasiaVitiv <vitiv.tuska@gmail.com>
2024-09-25 18:51:37 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -6,18 +6,15 @@ b = 6
 h = 0.2
 mn = 4.5
 mx = 5
-def calculate(x):
-    if x < mn and x >= a:
-        return 1 / math.sin(x ** 2)
 
-    elif x >= mn and x < mx:
-        return x + math.log(math.sqrt(x ** 7))
-
-    elif x >= mx and x <= b:
-        return math.log10((math.e ** x) + 4)
-
+while x <= b:
+    if a <= x < mn:
+        y = 1 / math.sin(x ** 2)
+    elif mn <= x < mx:
+        y = x + math.log(math.sqrt(x ** 7))
+    elif mx <= x <= b:
+        y = math.log10((math.e ** x) + 4)
     else:
-        return "Не входить в область значень"
-
-result = round(calculate(x), 4)
-print(f"Результат: {result}")
+        print("Не входить в область значень")
+    x += h
+print(y)
