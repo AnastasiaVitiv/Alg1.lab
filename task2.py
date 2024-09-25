628bdbe0321290d121c0c9e0b37b4eb0b313b760
AnastasiaVitiv <vitiv.tuska@gmail.com>
2024-09-25 18:50:27 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,17 +1,26 @@
 import math
 
 d = 0.001
-term = 0.00
+term = 0.0
 h = 0.1
 a = 3
 b = 4
 
-print(f"{'k':^10} {'x':^10} {'term':^15}")
+print(f"{'x':^10} {'term':^15}")
 
-k = 1
+
 x = a
-while (term > d or term == 0.00) and x <= b:
-    print(f"{k:^10} {x:^10.1f} {term:^15.5f}")
-    term += abs((1 / k) * (math.tan(x / (2 ** k))))
-    k += 1
-    x += h
+while x <= b:
+    mysum = 0
+    k = 1
+    term = (1 / k) * (math.tan(x / (2 ** k)))
+
+    while abs(term) > d:
+        mysum += term
+
+        term = abs((1 / k) * (math.tan(x / (2 ** k))))
+        k += 1
+
+    print(f"{x} {mysum}")
+    x = x + h
+    x = round(x, 4)
